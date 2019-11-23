c76bfef5351e541a7fc78665622bdea1bc24312f
eisenhaus335 <greenzone332@protonmail.com>
2019-11-23 18:02:06 +0700

    AAAAAA

--- agenda.js
+++ agenda.js
@@ -17,23 +17,24 @@ const agenda = new Agenda({
 })
 
 agenda.define('send news indonesia', async () => {
-    const news = await NewsApi.v2.topHeadlines({
-        country: 'id'
+        const news = await NewsApi.v2.topHeadlines({
+            country: 'id'
+        })
+        
+        webhook.send('Dibaca Beritanya')
+        webhook.send({
+            embeds: news.articles.map(article => ({
+                    url: article.url,
+                    thumbnail: {
+                        url: article.urlToImage,
+                    },
+                    title: article.title,
+                    description: article.description,
+                    timestamp: article.publishedAt
+                }))
+            })
     })
-    
-    webhook.send('Dibaca Beritanya')
-    webhook.send({
-        embeds: news.articles.map(article => ({
-                url: article.url,
-                thumbnail: {
-                    url: article.urlToImage,
-                },
-                title: article.title,
-                description: article.description,
-                timestamp: article.publishedAt
-            }))
-    })
-})
+
 
 (async function() {
     await agenda.start()
