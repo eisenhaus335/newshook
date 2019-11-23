7d31bce053ea6db8a1361b6d074d1f8498ee742a
eisenhaus335 <greenzone332@protonmail.com>
2019-11-23 16:34:12 +0700

    Tegar Kontol

---
+++ agenda.js
@@ -0,0 +1,54 @@
+console.log('Prepare to setup')
+
+const Agenda = require('agenda')
+const { MongoClient } = require('mongodb');
+
+
+const discord = require('discord.js')
+const webhook = new discord.WebhookClient(process.env.ID, process.env.TOKEN)
+
+const api = require('newsapi')
+const NewsApi = new api(process.env.NEWSAPI)
+
+async function run() {
+    const agenda = new Agenda({
+        db: {
+            address: process.env.MONGO
+        }
+    })
+    
+    agenda.define('send news indonesia', async () => {
+        const news = await NewsApi.v2.topHeadlines({
+            country: 'id'
+        })
+        webhook.send('Dibaca Beritanya')
+        webhook.send({
+            embeds: news.articles.map(article => ({
+                    url: article.url,
+                    thumbnail: {
+                        url: article.urlToImage,
+                    },
+                    title: article.title,
+                    description: article.description,
+                    timestamp: article.publishedAt
+                }))
+        })
+    })
+
+    await new Promise(resolve => agenda.once('ready', resolve));
+
+    const sendnews = agenda.create('send news indonesia')
+    await agenda.start()
+    await sendnews.repeatAt('7.00AM').save()
+    await sendnews.repeatAt('12.00AM').save()
+    await sendnews.repeatAt('5.00PM').save()
+    await sendnews.repeatAt('8.00PM').save()
+    console.log('Everything is done')
+}
+
+
+run().catch(error => {
+    console.error(error);
+    process.exit(-1);
+  });
+
