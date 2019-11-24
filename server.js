b744b4a3d9ee0a2f887392dcf012aa4a1d23c018
eisenhaus335 <greenzone332@protonmail.com>
2019-11-24 19:47:06 +0700

    API

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const port = process.env.PORT || 8080
 const agenda = require('./agenda')
 const Agendash = require('agendash')
 
+app.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      ctx.status = err.status || 500;
+      ctx.body = err.message;
+      ctx.app.emit('error', err, ctx);
+    }
+});
+
 const indexRoutes = require('./routes')
 app.use(indexRoutes)
 
--- worker.js
+++ worker.js
@@ -4,6 +4,7 @@ const discord = require('./discord')
 async function run() {
     await agenda.start()
     await discord.login(process.env.TOKEN)
+    console.log('Worker Ready')
 }
 
 run().catch(error => {
